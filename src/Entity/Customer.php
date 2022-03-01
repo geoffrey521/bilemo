a622b5193b351d98dde17e27fc7fdc4ca0fa5c38
geoffrey521 <calvino.geoffrey@gmail.com>
2022-03-01 15:43:53 +0100

    Feature #2 : create entities

---
+++ src/Entity/Customer.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CustomerRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+#[ORM\Entity(repositoryClass: CustomerRepository::class)]
+#[UniqueEntity(fields: ['society'])]
+#[UniqueEntity(fields: ['email'])]
+class Customer
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $society;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $contactFirstname;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $contactLastname;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $phone_number;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $email;
+
+    #[ORM\OneToMany(mappedBy: 'customer', targetEntity: User::class, orphanRemoval: true)]
+    private $users;
+
+    public function __construct()
+    {
+        $this->users = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSociety(): ?string
+    {
+        return $this->society;
+    }
+
+    public function setSociety(string $society): self
+    {
+        $this->society = $society;
+
+        return $this;
+    }
+
+    public function getContactFirstname(): ?string
+    {
+        return $this->contactFirstname;
+    }
+
+    public function setContactFirstname(?string $contactFirstname): self
+    {
+        $this->contactFirstname = $contactFirstname;
+
+        return $this;
+    }
+
+    public function getContactLastname(): ?string
+    {
+        return $this->contactLastname;
+    }
+
+    public function setContactLastname(?string $contactLastname): self
+    {
+        $this->contactLastname = $contactLastname;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?string
+    {
+        return $this->phone_number;
+    }
+
+    public function setPhoneNumber(string $phone_number): self
+    {
+        $this->phone_number = $phone_number;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getUsers(): Collection
+    {
+        return $this->users;
+    }
+
+    public function addUser(User $user): self
+    {
+        if (!$this->users->contains($user)) {
+            $this->users[] = $user;
+            $user->setCustomer($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(User $user): self
+    {
+        if ($this->users->removeElement($user)) {
+            // set the owning side to null (unless already changed)
+            if ($user->getCustomer() === $this) {
+                $user->setCustomer(null);
+            }
+        }
+
+        return $this;
+    }
+}
